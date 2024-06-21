1bd1f7db9fce4cf8a6a3aa6e06f0cc8c48f5875a
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-06-21 20:34:27 +0500

    Update main.js

--- Fetch & async Await/main.js
+++ Fetch & async Await/main.js
@@ -5,7 +5,7 @@
 
  Question: What is API?
  Answer: The API client acts as a bridge between the server and the client, facilitating the exchange of data.
-*/ /*
+*/ 
 let url = "https://jsonplaceholder.typicode.com/posts/1"
 
 let fetchData = fetch(url).then((response)=>(response.json()))
@@ -13,7 +13,7 @@ let fetchData = fetch(url).then((response)=>(response.json()))
 .catch(error=>console.log(error))
 
 console.log(fetchData);
-*/
+
 //--------------------------------------------------------------------------------------------------------//
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
